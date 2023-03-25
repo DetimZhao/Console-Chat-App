d4170e4d3dfffeb0ea42746051fdafeea14238d4
isaaclugo <isaaclugo@localhost>
2023-03-24 18:12:31 -0700

    test commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     } // Detim's Mac
-    // Hi im Isaac! 
+    // Hi im Isaac! WHAT THE FUCK
 
 }
