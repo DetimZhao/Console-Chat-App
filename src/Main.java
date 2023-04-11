ff95c1fff3dd686284b6a7fa6537538658c11e8a
DetimZhao <detimzhao8668@gmail.com>
2023-04-11 12:53:49 -0700

    updated Main, working on start menu

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    } // Detim's Mac
+        displayMenu();
+    }
 
-    Connection c = null;
-    Statement s = null;
+    public static void displayMenu() {
+        System.out.println("Welcome to the Chat App!\n");
+        System.out.println("Please select from the following options:");
+        System.out.println("(R)egister, (L)ogin, (Q)uit");
+        System.out.println("-----------------------------------------");
+    }
 
-    // Create Table
-//    try {
-//        stat = c.createStatement();
-//        String sql = "CREATE TABLE "
-//    }
+    public static void checkUserOption() {
+        if(userDecision().equalsIgnoreCase("R")
+        || userDecision().equalsIgnoreCase("R")
+        || userDecision().equalsIgnoreCase("R")) {
+            // need to change
+        }
+    }
+
+    public static String userDecision() {
+        return input.next();
+    }
+
 }
