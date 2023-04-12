9d9a7f3deb94e43b8fda892a7e2a5dcd147ada23
DetimZhao <detimzhao8668@gmail.com>
2023-04-11 21:54:06 -0700

    updated Main, set up menu

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,40 @@ public class Main {
 
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        displayMenu();
+        System.out.println("Welcome to the Chat App!\n");
+        userDecision();
     }
 
     public static void displayMenu() {
-        System.out.println("Welcome to the Chat App!\n");
         System.out.println("Please select from the following options:");
         System.out.println("(R)egister, (L)ogin, (Q)uit");
         System.out.println("-----------------------------------------");
     }
 
-    public static void checkUserOption() {
-        if(userDecision().equalsIgnoreCase("R")
-        || userDecision().equalsIgnoreCase("R")
-        || userDecision().equalsIgnoreCase("R")) {
-            // need to change
+    public static void userDecision() {
+        displayMenu();
+        String userInput = input.nextLine();
+        while(true) {
+            switch(userInput.toLowerCase()) {
+                case "r", "register":
+                    System.out.println("r or register");
+                    break;
+                case "l", "login":
+                    System.out.println("l or login");
+                    break;
+                case "q", "quit":
+                    System.out.println("q or quit");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid! Try again.\n");
+                    displayMenu();
+                    userInput = input.nextLine();
+                    continue;
+            }
+            break; // Exit while loop once user picks valid choice
         }
-    }
 
-    public static String userDecision() {
-        return input.next();
     }
 
 }
